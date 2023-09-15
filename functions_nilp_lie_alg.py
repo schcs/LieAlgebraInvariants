ae2f2fefdf1ed3af020b384bd651f317767d8e09
Csaba Schneider <csaba.schneider@gmail.com>
2023-09-15 11:18:35 -0300

    added function lie_algebra_upper_triangular_matrices

--- functions_nilp_lie_alg.py
+++ functions_nilp_lie_alg.py
@@ -519,6 +519,58 @@ def lie_algebra_strict_upper_triangular_matrices(n):
     L = LieAlgebra(QQ, dict_lie, names = keys)
     return L
 
+
+# the solvable Lie algebra of upper triangular matrices
+
+def lie_algebra_upper_triangular_matrices( n ):
+
+    # the dimension of the algebra 
+    dimL = n*(n+1)//2
+
+    # this function calculates the product of two elementary matrices. 
+    # the first matrix has 1 at position i, j; the second has 1 at position k,l
+    # this lambda function returns the position where the product has one;
+    # of no such position exists, then the output is ()
+    prod_func = lambda i,j,k,l: (j==k)*(i,l)
+
+    # the standard basis is labelled as xij where i, j indicates the non-zero entry
+    basis_labels = [(i+1,j+1) for i in range( n ) for j in range( i, n )]
+
+    # we sort the basis labels so that they correspond to the derived series of L
+    # this helps in the invariant field computation
+    basis_labels.sort( key = lambda l : l[1]-l[0], reverse = True  )
+
+    # prod_dict is the dictionary for the bracket on L
+    prod_dict = {}
+    # this is just the list of strings to name the basis elements of L
+    basis_names = [ 'x'+str(l[0])+str(l[1]) for l in basis_labels ]
+
+    # we run i, j over indices 0<= i < j <= dimL-1
+    for i in range( dimL ):
+        # get the basis label for the first basis element b1 
+        x, y = basis_labels[i]
+        for j in range( i+1, dimL ):
+            # basis label for the second basis element b2 
+            u, v = basis_labels[j]
+            # we calculate b1*b2 and b2*b1 using the lambda expression above
+            pr1, pr2 = prod_func( x, y, u, v ), prod_func( u, v, x, y )
+            
+            # we calculate the entry to be added to the product dictionary
+            prod_dict_entry = {}            
+
+            if pr1 != ():
+                prod_dict_entry['x'+str(pr1[0])+str(pr1[1])] = 1
+            if pr2 != ():
+                prod_dict_entry['x'+str(pr2[0])+str(pr2[1])] = -1
+
+            # add the entry to the prod dictionary
+            if prod_dict_entry != {}:
+                prod_dict[('x'+str(x)+str(y), 'x'+str(u)+str(v))] = prod_dict_entry
+            
+    # return the Lie algebra
+    return LieAlgebra(QQ, prod_dict, basis_names )
+
+
 def standard_filiform_lie_algebra( n ):
 
     vars = [ 'y'+str(k) for k in range( n-1 ) ] + ['x']
