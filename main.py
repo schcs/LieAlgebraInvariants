42be765e3d3851ec945d3d6eb436d113002aa741
Igor Silva <silvamigor@gmail.com>
2024-05-28 14:21:21 -0300

    função para calcular os invariantes de uma derivação oriunda de uma matriz

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from sage.rings.number_field.splitting_field import SplittingFieldAbort
 load("membership_pols.py")
 load("functions_nilp_lie_alg.py")
 load("invariants_nilp_lie_alg.py")
-load( "characteristics.py" )
+load("characteristics.py")
+load("matrix_derivation_invariants.py")
