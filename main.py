1fcbe9f280b9e2170c4c617fbb780d6bc47cafa6
Csaba Schneider <csaba.schneider@gmail.com>
2025-03-11 10:47:02 -0300

    changes in main

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
 load("characteristics.py")
-<<<<<<< HEAD
-load("membership_pols.py")
-load("functions_lie_algebra.py")
-load("derivations_lie_algebra.py")
-load("example_nilpotent_lie_algebra.py")
-load("nilpotent_lie_algebra_dim6.py")
-load("solvable_lie_algebra_dim4.py")
-load("matrix_derivation_invariants.py")
-load("invariants_rational_lie_algebra.py")
-=======
 #load("matrix_derivation_invariants.py")
 #load("solvable_invariants.py")
->>>>>>> csaba_branch
